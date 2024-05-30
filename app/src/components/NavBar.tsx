8994e5d2f42258565bed92e22836271ae759e9bc
Vincent DENIEL <vincentdnl@users.noreply.github.com>
2024-05-30 15:00:26 +0200

    fixed bug shadow

--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -4,11 +4,11 @@ import {ChatsHistories} from "./ChatsHistories.tsx"
 
 export const NavBar = () => {
     return (
-        <div className={"flex flex-col container gap-1 shadow-xl"}>
+        <div className={"flex flex-col container gap-1"}>
             <div className={"text-white opacity-70 text-right"}>
                 <span>{environment.VITE_SERVICE_URL}</span>
             </div>
-            <div className={"flex bg-white bg-opacity-70 justify-between px-6 py-2 rounded-t-xl"}>
+            <div className={"flex bg-white bg-opacity-70 justify-between px-6 py-2 rounded-t-xl shadow-xl"}>
                 <div className={"flex flex-row items-center gap-6"}>
                     <ChatsHistories/>
                 </div>
