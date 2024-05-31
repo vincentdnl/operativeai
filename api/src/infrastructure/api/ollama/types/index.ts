6674d8cd069319c901d3438bb8b0b2e3c25dcccd
Vincent DENIEL <vincentdnl@users.noreply.github.com>
2024-05-31 10:49:19 +0200

    cleaning

--- api/src/infrastructure/api/ollama/types/index.ts
+++ api/src/infrastructure/api/ollama/types/index.ts
@@ -56,25 +56,3 @@ export interface ChatResponse {
     eval_count: number;
     eval_duration: number;
 }
-
-interface Choice {
-    index: number;
-    message: Message,
-    finish_reason: string;
-}
-
-interface Usage {
-    prompt_tokens: number;
-    completion_tokens: number;
-    total_tokens: number;
-}
-
-interface ChatCompletion {
-    id: string;
-    object: string;
-    created: number;
-    model: string;
-    system_fingerprint: string;
-    choices: Choice[];
-    usage: Usage;
-}
